df3b407eb96beb8a7e077a91bd4c836b5596c3b9
mfudo <marshalludo@gmail.com>
2014-05-11 16:06:31 -0700

    Changed subsetting

--- plot2.R
+++ plot2.R
@@ -7,8 +7,11 @@ pwr$Date <- as.Date(pwr$Date, format="%d/%m/%Y")
 pwr$Time <- strptime(pwr$Time, "%H:%M:%S")
 
 pwr_sub <- subset(pwr, pwr$Date == as.Date("2007/02/01") | pwr$Date == as.Date("2007/02/02"))
+pwr_sub <- pwr_sub[order(pwr_sub$Time),]
 xrange = range(pwr_sub$Time)
 yrange = range(pwr_sub$Global_active_power)
 png("plot2.png", width = 480, height = 480)
-plot(pwr_sub$Time, pwr_sub$Global_active_power, ylab="Global Active Power (kilowatts)") 
+plot(xrange, yrange, type="l", xaxt="n", xlab="", ylab="Global Active Power (kilowatts)") 
+lines(pwr_sub$Time, pwr_sub$Global_active_power)
+axis.POSIXct(1, at=seq(pwr_sub$Time[0], by="day"), format="%a", labels=T)
 dev.off()
