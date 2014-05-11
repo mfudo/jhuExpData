943b1d645f6468a8855017c99b4acf7effcc6c34
mfudo <marshalludo@gmail.com>
2014-05-11 14:21:53 -0700

    plot 1

--- plot1.R
+++ plot1.R
@@ -1,9 +1,11 @@
 # Global Active Power histogram
+# Plot 1
 
 setwd("~/jhuExpData")
-pwr <- read.table("hs_pwr.txt", sep=";", stringsAsFactors=F, na.string=c("?"))
-pwr$Date <- as.Date(pwr$Date)
+pwr <- read.table("hs_pwr.txt", sep=";", header=T, stringsAsFactors=F, na.string=c("?"))
+pwr$Date <- as.Date(pwr$Date, format="%d/%m/%Y")
 pwr$Time <- strptime(pwr$Time)
 
-pwr_sub <- pwr[pwr$Date == c("2007-02-01","2007-02-02"),]
+pwr_sub <- pwr[pwr$Date == c("2007/02/01","2007/02/02"),]
 
+with(pwr_sub, hist(pwr_sub$Global_active_power, main="Global Active Power", breaks=12, col="red", xlab="Global Active Power (kilowatts)", ylab="Frequency"))
